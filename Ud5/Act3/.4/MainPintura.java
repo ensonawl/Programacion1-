b2a7641dcad71eb2502f2fb1aed0c826ea5b9979
ensonawl <enzofernandez2007@gmail.com>
2026-01-29 08:15:34 +0100

    Implement main method to calculate paint needed

--- Ud5/Act3/.4/MainPintura.java
+++ Ud5/Act3/.4/MainPintura.java
@@ -1,1 +1,10 @@
+void main() {
+    Pintura pintura = new Pintura(250);
+    Forma rectangulo = new Rectangulo(20, 35);
+    Forma esfera = new Esfera(15);
+    Forma cilindro = new Cilindro(10,30);
 
+    System.out.println(pintura.pinturaNecesaria(rectangulo));
+    System.out.println(pintura.pinturaNecesaria(esfera));
+    System.out.println(pintura.pinturaNecesaria(cilindro));
+}
